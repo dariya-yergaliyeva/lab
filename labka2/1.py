a233237067efe0a4c321dc01831b6cbda697fe04
dariya-yergaliyeva <d_yergaliyeva@kbtu.kz>
2025-01-22 09:32:09 +0500

    meow

---
+++ labka2/1.py
@@ -0,0 +1,19 @@
+a = 200
+b = 33
+
+if b > a:
+  print("b is greater than a")
+else:
+  print("b is not greater than a")
+
+bool(False)
+bool(None)
+bool(0)
+bool("")
+bool(())
+bool([])
+bool({})
+
+bool("abc")
+bool(123)
+bool(["apple", "cherry", "banana"])
