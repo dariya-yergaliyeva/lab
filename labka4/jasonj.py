af141bb0703f2d9407ee3c3d309cdf1e967a5d12
dariya-yergaliyeva <dariaergalieva92@gmail.com>
2025-02-12 18:28:49 +0500

    f

--- labka4/jasonj.py
+++ labka4/jasonj.py
@@ -3,4 +3,14 @@ import json
 info = 'sample-data.json'
 with open(info, "r") as file:
     data = json.load(file)
-sorts=data["imdata"]
+out=[]
+out.append("interface status")
+out.append("="*90)
+out.append(f"{'DN:<50'} {'description:<20'} {'speed':<10} {'mtu':<10}")
+out.append("-"*90)
+for datas in data["imdata"]:
+    m=datas["l1PhysIf"]["attributes"]
+    out.append(f"{m['dn']:<50} {m['pathSDescr']:<20} {m['speed']:<10} {m['mtu']:<10}")
+
+for i in out:
+    print(i)
